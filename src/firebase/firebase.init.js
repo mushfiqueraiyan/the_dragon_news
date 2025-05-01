33e02252e62979e3b9f1659347639dcf8896085d
mushfiqueraiyan <mushfiqueraiyan1@gmail.com>
2025-05-01 23:38:44 +0600

    hide some private information from github

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBOxH1sSk2qE3Qs8YRdFZYAftf7x22HWSM",
-  authDomain: "dragon-news-910fe.firebaseapp.com",
-  projectId: "dragon-news-910fe",
-  storageBucket: "dragon-news-910fe.firebasestorage.app",
-  messagingSenderId: "928386785685",
-  appId: "1:928386785685:web:18f1aad1e384820789169c",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
